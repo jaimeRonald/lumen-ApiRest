ba90ab68e5a5449a7ec98fcc13580ca0dbe48de2
Ronaldo4040 <jaimevisitacion@innovahora.com>
2022-07-29 16:53:26 -0500

    una agregado mas

--- lumen_apiRest/app/Http/Controllers/LibroController.php
+++ lumen_apiRest/app/Http/Controllers/LibroController.php
@@ -5,12 +5,16 @@ use Illuminate\Http\Request;
 use App\Models\Libro;
 use Symfony\Component\Console\Input\Input;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class LibroController extends Controller{
 
-    public function index(Request $req){
-        // $datosLibro=Libro::all();
-        return response()->json($req);
+    public function index(){
+        $libro= new Libro();
+        // $products = DB::table('libros')->get();
+        $datosLibro=libro::all();
+
+        return response()->json($datosLibro);
     }
 
     public function guardar(Request $req){
@@ -28,19 +32,12 @@ class LibroController extends Controller{
        
         return response()->json($nuevoNombre);
     }
-    public function update(Request $req){
-        $libro= new Libro();
-        $req->file('imagen');// la infomracion se almacena con esto
-        // if($req->hasFile('imagen')){
-        //     $nombreArchivoOrignial=$req->file('imagen')->getClientOriginalName();
-        //     $nuevoNombre=Carbon::now()->timestamp."-".$nombreArchivoOrignial;
-        //     $carpetaDestino="./upload/";
-        //     $req->file('imagen')->move($carpetaDestino,$nuevoNombre);// mpvemps el nuevo nombre de la imagen a la caerpeta uploadas
-        //     $libro->titulo=$req->titulo;
-        //     $libro->imagen=ltrim($carpetaDestino,".").$nuevoNombre;// guardamos en el campo imagen la ruta de laimagen y nombre            $libro->save();
-        //     $libro->save();
-        // }
-       
-        return response()->json($req);
+
+    public function leer(Request $req){
+        $libro=new Libro();
+        $todos=DB::table('Libro') // ese codigo aun no esta operativo
+        // ->where('id',$req)
+        ->get();
+        return  response()->json($todos);
     }
 }
